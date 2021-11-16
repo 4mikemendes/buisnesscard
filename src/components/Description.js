536ab772a20ec2022e4d3b612d5346260f0a2fd4
mike mendes <4mikemendes@gmail.com>
2021-11-16 13:13:48 +0100

    small ajus

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,9 +4,15 @@ export default function Description() {
     return(
         <div>
         <h3>About</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </p>
+        <p>Lorem Ipsum has been the industry's 
+            standard dummy text ever since the 1500s, when an unknown printer 
+            took a galley of type and scrambled it to make a type specimen book. 
+         </p>
         <h3>Interests</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </p>
+        <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book. 
+            leap into electronic </p>
         </div>
     )
 }
